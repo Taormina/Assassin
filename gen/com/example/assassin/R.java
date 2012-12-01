f0b87b0bb62a8f340a9ec39b7e2e5f14dafa5fc1
Anthony Taormina <anthony.m.taormina@gmail.com>
2012-12-01 00:08:03 -0500

    Pictures work

--- gen/com/example/assassin/R.java
+++ gen/com/example/assassin/R.java
@@ -15,10 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int imageView1=0x7f070002;
-        public static final int menu_settings=0x7f070003;
-        public static final int name=0x7f070000;
-        public static final int register=0x7f070001;
+        public static final int change_pic=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int name=0x7f070001;
+        public static final int register=0x7f070000;
+        public static final int user_image=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,6 +29,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int change_pic=0x7f040006;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int player_name=0x7f040005;
--- src/com/example/assassin/MainActivity.java
+++ src/com/example/assassin/MainActivity.java
@@ -1,35 +1,78 @@
 package com.example.assassin;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	public String AppName = "Assassin";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_main);
+        
+        Button registerButton, changePicButton;
  
-        Button registerButton = (Button) findViewById(R.id.register);
+        registerButton = (Button) findViewById(R.id.register);
         registerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				EditText et = (EditText) findViewById(R.id.name);
+		        EditText et = (EditText) findViewById(R.id.name);
 				Editable name = et.getText();
-				Log.e("App",name.toString());
+				
+			}
+		} );
+        
+        changePicButton = (Button) findViewById(R.id.change_pic);
+        changePicButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);		
 			}
 		} );
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, 
+    	       Intent imageReturnedIntent) {
+    	    super.onActivityResult(requestCode, resultCode, imageReturnedIntent); 
+
+            Uri selectedImage = imageReturnedIntent.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(
+                               selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+            ImageView iv = (ImageView) findViewById(R.id.user_image);
+            iv.setImageBitmap(yourSelectedImage);
+ 
+    	}
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
